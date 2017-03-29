e0c229818aedaedce685316213b0b7c97a45536b
s3627014 <s3627014@student.rmit.edu.au>
2017-03-29 11:56:01 +1100

    Testing eclipse GITHUB

--- src/main/Initialise.java
+++ src/main/Initialise.java
@@ -3,14 +3,14 @@ package main;
 class Initialise {
 	public static void main(String [ ] args)
 	{
-		DBConnect();
+		//DBConnect();
 		createMenu();
 	}
 
 	public static void createMenu() {
 		System.out.println("*************** Course Management System ***************\n");
 		System.out.println("1.   Login");
-		System.out.println("3.   Exit\n");
+		System.out.println("3.   ExitTESTING IF I CAN PUSH FROM ECLIPSE\n");
 		System.out.println("**********************************************************");
 	}
 }
