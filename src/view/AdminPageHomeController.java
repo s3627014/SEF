6e6de3b07626e70955275fe502f5beecbdb5c3a2
Tim <s3588689@student.rmit.edu.au>
2017-05-05 19:47:38 +1000

    Admin Page fxml and controller java
    Created a skeleton for now. Ill add more once I can get the DB working
    from home

---
+++ src/view/AdminPageHomeController.java
@@ -0,0 +1,29 @@
+package view;
+
+import application.MainApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AdminPageHomeController {
+	/*@FXML
+	private Button courseButton;
+
+	private String userID;
+	 MainApp main = new MainApp();
+	 @FXML
+	    private void initialize() {
+	    }
+	 public void CourseClicked() throws Exception{
+	    	main.showStudentCoursePage();
+	    	
+	    }
+	 public void EnrolClicked() throws Exception{
+	    	main.showStudentEnrolPage();
+	    	
+	    }
+	 public void setUserID(String userID) {
+			this.userID = userID;
+			System.out.println("Setting the id as " + userID);
+		}
+*/
+}
